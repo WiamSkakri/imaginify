3fe1db4e3bf263386da29d93e95c785950076f7a
Wiam Skakri <wxs428@case.edu>
2024-08-06 15:10:52 +0100

    implement clerk webhooks

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,27 +1,41 @@
-import { Document, Schema, model, models } from "mongoose";
-
-interface IUser extends Document {
-    clerkID: string;
-    email: string;
-    username: string;
-    photo: string;
-    firstName?: string;
-    lastName?: string;
-    planId?: number;
-    creditBalance?: number;
-}
+import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-    clerkID: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
-    photo: { type: String, required: true },
-    firstName: { type: String },
-    lastName: { type: String },
-    planId: { type: Number, default: 1 },
-    creditBalance: { type: Number, default: 10 },
-  });
-  
-  const User = models?.Image || model('User', UserSchema);
-  
-  export default User;
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  planId: {
+    type: Number,
+    default: 1,
+  },
+  creditBalance: {
+    type: Number,
+    default: 10,
+  },
+});
+
+const User = models?.User || model("User", UserSchema);
+
+export default User;
