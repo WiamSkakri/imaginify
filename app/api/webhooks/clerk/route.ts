312653a2bea571220852ba25bbf2dfb7c9da3ac9
Wiam Skakri <wxs428@case.edu>
2024-08-07 09:09:18 +0100

    correction to utils

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,7 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { Webhook } from "svix";
+
+import { createUser } from "@/app/lib/actions/user.action";
